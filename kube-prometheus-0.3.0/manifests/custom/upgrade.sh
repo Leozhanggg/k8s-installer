fb4b7f435023412f655599f9bd8833e738bb0f8a
leozhanggg <278869269@qq.com>
2020-08-12 17:46:45 +0800

    升级elastic到7.7.1并添加prometheus自定义配置

---
+++ kube-prometheus-0.3.0/manifests/custom/upgrade.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# upgrade alertmanager configuration
+kubectl create secret generic alertmanager-main --from-file=alertmanager.yaml -n monitoring --dry-run -oyaml > alertmanager-secret.yaml
+kubectl apply -f alertmanager-secret.yaml
+
+# upgrade scrape configs
+kubectl create secret generic additional-scrape-configs --from-file=prometheus-additional.yaml -n monitoring --dry-run -oyaml > additional-scrape-configs.yaml
+kubectl apply -f additional-scrape-configs.yaml
+
+# upgrade prometheus rules
+kubectl apply -f prometheus-additional-rules.yaml
+kubectl apply -f prometheus-rules.yaml
+
+# upgrade prometheus configuration
+kubectl apply -f prometheus-prometheus.yaml
